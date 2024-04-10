cf8ffcf576dd490b8a63c080cfbc1b9042073bcc
Jesus Reyes <luisdejesus200122@gmail.com>
2024-04-10 00:22:54 -0600

    Feat: Create Trabajador Controller Added

--- src/controllers/trabajadores/trabajadores.controller.ts
+++ src/controllers/trabajadores/trabajadores.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { TrabajadoresModel } from '../../models/trabajadores';
+import bycrypt from 'bcryptjs';
 export class Controller {
   get = async (req: Request, res: Response) => {
     const trabajadores = await TrabajadoresModel().findAll();
@@ -9,4 +10,24 @@ export class Controller {
       trabajadores,
     });
   };
+
+  crearTrabajador = async (req:  Request, res: Response) => {
+    try {
+    const {body} = req;
+    const salt = await bycrypt.genSalt(10);
+    body.password  = bycrypt.hashSync(body.password, salt);
+    const trabajador = await TrabajadoresModel().create(body);
+
+    res.json({
+      ok: true,
+      trabajador
+    })
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        ok: false,
+        msg: `Hable con el administrador: ${error}`
+      });
+    }
+  }
 }
