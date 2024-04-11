4ed6522917765eead6ca3c28c9c6e8fafb340bf5
Jesus Reyes <luisdejesus200122@gmail.com>
2024-04-11 17:11:03 -0600

    Feat: Crear examenes Resultados controller

--- src/routes/examenes/examenes.routes.ts
+++ src/routes/examenes/examenes.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 const controller = new Controller();
 router.get('/', controller.get);
 router.get('/resultados', controller.getResultadosExamenes);
+router.post('/resultados', controller.crearExamenResultado);
 router.post('/', controller.crearExamenResultado);
 
 export default router;
